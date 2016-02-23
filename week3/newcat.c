26dffec84d9883c663dca988fd83428fc11c5548
doviettung96 <tunga9td@gmail.com>
2016-02-23 22:39:44 +0700

    mycat

---
+++ week3/newcat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char** argv)
+{
+  int max;
+  max = atoi(argv[2]);
+    
+  FILE* fptr1;
+  char buff[max + 1];
+  int num;
+    
+  if((fptr1 = fopen(argv[1], "rb")) == NULL)
+    printf("Can nor open the file %s\n", argv[1]);
+  else
+    {
+      num = fread(buff, sizeof(char), max, fptr1);
+      buff[num * sizeof(char)] = '\0';
+      printf("%s\n", buff);
+    }
+}
