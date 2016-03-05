46c99c171262de6c097d6c8b9dedec77fede7c68
doviettung96 <tunga9td@gmail.com>
2016-03-05 13:41:55 +0700

    files compare

--- week5/filecp.c
+++ week5/filecp.c
@@ -10,6 +10,7 @@ void main(int argc, char** argv) {
 	int dif[n];
 	int count = 0;
 	char a[50], b[50];
+	int first = n;
 
 	if ((f = fopen(argv[1], "r")) == NULL)
 	{
@@ -30,16 +31,22 @@ void main(int argc, char** argv) {
 		fscanf(p, "%[^\n]\n", b);
 		//printf("%s\n%s\n",a, b);
 		if (strcmp(a, b) != 0)
+		{
 			dif[count] = 1;
+			printf("The line where %s and %s differ:\n %s\n%s\n", argv[1], argv[2], a, b);
+
+		}
 		count++;
 	}
 
 	for (int i = 0; i <= count; ++i)
 		if (dif[i] == 1)
 		{
-			//printf("The first line where %s and %s differ: %d", argv[1], argv[2], dif[0]);
-			printf("The line where %s and %s differ: %d\n", argv[1], argv[2], i + 1);
+			if (i < first)
+				first = i;
 		}
-fclose(f);
-fclose(p);
+	printf("The first line where %s and %s differ: %d\n", argv[1], argv[2], first + 1);
+
+	fclose(f);
+	fclose(p);
 }
