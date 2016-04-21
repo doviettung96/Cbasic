cdd2c41665dfe28956a404ca0983faa808aad78c
doviettung96 <tunga9td@gmail.com>
2016-04-21 20:59:58 +0700

    homework week9

--- week9/expression.c
+++ week9/expression.c
@@ -1,50 +1,87 @@
 #include "bintree.h"
 #include "stacklib.h"
 
-#define MAX 20
+#define MAX 30
 void display(tree_type tree);
 int isOperator(char token);
+int isOperand(char token);
 int openLiteral(char token);
 int closeLiteral(char token);
+int popOperator(char poped, char consider);
 int matchLiteral(char x, char y);
 
 void main(int argc, char **argv)
 {
 	FILE *f;
-	int i, j = 0;
+	int i = 0, j = 0;
 	TNode *p;
 	stack_type *stack;
-
+	char *temp;
 	char postfix[MAX];
 	char infix[MAX];
 	tree_type tree = NULL;
 	makeNullTree(&tree);
 	stack = iniStack(stack);
 
+	temp = (char *)malloc(sizeof(char));
 	printf("Type in an infix expression\n");
 	scanf("%s", infix);
+
+
 	for (i = 0; i < strlen(infix); ++i)
 	{
 		if (openLiteral(infix[i]))
-			push(infix[i], stack);
-		if (closeLiteral(infix[i]))
+			push(&infix[i], stack);
+
+		else if (closeLiteral(infix[i]))
 		{
-			while(!openLiteral(postfix[j])) {
-				postfix[j] = pop(stack);
+			while (1)
+			{
+				temp = (char *)pop(stack);
+				if (openLiteral(*temp))
+					break;
+				postfix[j] = *temp;
 				++j;
 			}
 		}
-		if(isOperator(infix[i]))
 
+		else if (isOperator(infix[i]))
+		{
+			while (!empty(stack))
+			{
+				temp = (char*) stack->top->element;
+				if (popOperator(*temp, infix[i]))
+					break;
+				temp = (char *)pop(stack);
+				postfix[j] = *temp;
+				++j;
+			}
+			push(&infix[i], stack);
+		}
+		else if (isOperand(infix[i]))
+		{
+			postfix[j] = infix[i];
+			++j;
+		}
 	}
 
+	while (!empty(stack))
+	{
+		temp = (char *)pop(stack);
+		postfix[j] = *temp;
+		++j;
+	}
 
-	printf("\nThe tree in preorder is: \n");
-	preOrder(tree, display);
-	printf("\nThe tree in inorder is: \n");
-	inOrder(tree, display);
-	printf("\nThe tree in postorder is: \n");
-	postOrder(tree, display);
+	for (i = 0; i < strlen(postfix); ++i)
+		printf("%c ", postfix[i]);
+	printf("\n");
+
+	// printf("\nThe tree in preorder is: \n");
+	// preOrder(tree, display);
+	// printf("\nThe tree in inorder is: \n");
+	// inOrder(tree, display);
+	// printf("\nThe tree in postorder is: \n");
+	// postOrder(tree, display);
 	// printf("\nTotal nodes of tree is: %d\n", countNode(tree));
 	// printf("Total leaves of tree is: %d\n", countLeaves(tree));
 	// printf("The height of the tree is: %d\n", height(tree));
@@ -59,11 +96,27 @@ void display(tree_type tree) {
 	printf("%s ", (char *)(tree->info));
 }
 
-int isOperator(char token) {
-	if (token == '+' || token == '-') 
+int isOperand(char token) {
+	if (token <= '9' && token >= '0')
 		return 1;
-	if(token == '*' || token == '/')
+	return 0;
+}
+
+int isOperator(char token) {
+	if (token == '+' || token == '-')
+		return 1;
+	if (token == '*' || token == '/' || token == '%')
 		return 2;
+	if (token == '^')
+		return 3;
+	return 0;
+}
+
+//if the operator meet some requirement, they will be pop
+int popOperator(char poped, char consider)
+{
+	if (isOperator(poped) < isOperator(consider) || (isOperator(poped) == 3 && isOperator(consider) == 3))
+		return 1;
 	return 0;
 }
 
@@ -79,7 +132,6 @@ int closeLiteral(char token) {
 	return 0;
 }
 
-//check if the literal is in pair
 int matchLiteral(char x, char y)
 {
 	if (x == '(' && y == ')') return 1;
